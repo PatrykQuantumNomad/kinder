6a5d8183022bd8a136920d1c6b69959514bef2f0
Patryk Golabek <pgolabek@gmail.com>
2026-03-01 15:44:22 -0500

    test(08-01): add failing tests for all-false addon guard and non-addon defaults
    - TestSetDefaultsCluster_AddonFields: proves all-false and zero-value addons should be preserved
    - TestSetDefaultsCluster_NonAddonDefaults: proves Name, Nodes, IPFamily still default correctly
    - Tests currently FAIL because all-false guard in SetDefaultsCluster overrides to all-true

---
+++ pkg/internal/apis/config/default_test.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import "testing"
+
+func TestSetDefaultsCluster_AddonFields(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name       string
+		input      Addons
+		wantAddons Addons
+	}{
+		{
+			name: "all addons explicitly false remain false",
+			input: Addons{
+				MetalLB: false, EnvoyGateway: false,
+				MetricsServer: false, CoreDNSTuning: false, Dashboard: false,
+			},
+			wantAddons: Addons{
+				MetalLB: false, EnvoyGateway: false,
+				MetricsServer: false, CoreDNSTuning: false, Dashboard: false,
+			},
+		},
+		{
+			name:       "zero-value addons remain zero-value",
+			input:      Addons{},
+			wantAddons: Addons{},
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			c := &Cluster{Addons: tc.input}
+			SetDefaultsCluster(c)
+			if c.Addons != tc.wantAddons {
+				t.Errorf("SetDefaultsCluster() Addons = %+v, want %+v", c.Addons, tc.wantAddons)
+			}
+		})
+	}
+}
+
+func TestSetDefaultsCluster_NonAddonDefaults(t *testing.T) {
+	t.Parallel()
+	c := &Cluster{}
+	SetDefaultsCluster(c)
+	if c.Name == "" {
+		t.Error("SetDefaultsCluster should set default Name")
+	}
+	if len(c.Nodes) == 0 {
+		t.Error("SetDefaultsCluster should set default Nodes")
+	}
+	if c.Networking.IPFamily == "" {
+		t.Error("SetDefaultsCluster should set default IPFamily")
+	}
+}
